8d0140a65cbf6f69a81c5f28fd91790f28a09bd8
AVNV2201 <abhinav.cs2201@gmail.com>
2020-12-07 01:03:37 +0530

    student routes are done :)

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/profileMS';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/profileService';
 
-mongoose.connect( secret.dbUrl ,{
+mongoose.connect( dbUrl ,{
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
@@ -23,8 +23,12 @@ mongoose.connect( secret.dbUrl ,{
             console.log(err);
       });
 
+const studentRoutes = require('./routes/student')
+
 app.use( methodOverride('_method') );
 app.use( express.json() );
+
+app.use('/student', studentRoutes);
 
 app.use( ( req, res ) => {
       res.status(404).json({});
--- seeds/student.js
+++ seeds/student.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const Student = require('../models/student')
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/profileMS';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/profileService';
 
-mongoose.connect( secret.dbUrl ,{
+mongoose.connect( dbUrl ,{
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
@@ -15,3 +15,46 @@ mongoose.connect( secret.dbUrl ,{
             console.log('Something went wrong');
             console.log(err);
       });
+
+studentList = [
+      {
+            name: 'Abhinav Giri',
+            gender: 'male',
+            regNo: '20174045',
+            email: 'abhinav.cs@gmail.com',
+            dob: '1999/03/23'
+      },
+      {
+            name: 'Surya Bhai',
+            gender: 'male',
+            regNo: '20174003',
+            email: 'suryabhai.cs@gmail.com',
+            dob: '1999/07/13'
+      },
+      {
+            name: 'Swayam 007',
+            gender: 'male',
+            regNo: '20175062',
+            email: 'swayam.ece@gmail.com',
+            dob: '2000/02/20'
+      },
+      {
+            name: 'Hemant Sir',
+            gender: 'male',
+            regNo: '20154000',
+            email: 'hemantsir.cs@walmart.com',
+            dob: '1997/01/01'
+      }
+]
+
+const insertStudentList = () => {
+      Student.insertMany( studentList )
+      .then( s => {
+            console.log(s);
+      })
+      .catch( err => {
+            console.log(err);
+      })
+}
+
+insertStudentList();
